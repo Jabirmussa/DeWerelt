79b921e91e877bda6e79dd47a4eaa6a3b7e42aaf
Growskills <jabirmussa185@gmail.com>
2025-03-28 12:12:03 +0200

    Update styles and scripts for improved header functionality and layout: add live server settings, adjust button styles, enhance header responsiveness, and implement new swiper components.

--- js/script.js
+++ js/script.js
@@ -1,67 +1,92 @@
 // // Header scroll
-// function fixHeaderOnScroll() {
-//     const header = document.querySelector('header');
+const header = document.querySelector('header');
+if(header){
+
+  function fixHeaderOnScroll() {
+    
+      if (window.scrollY >= 1) {
+          header.classList.add('fixed');
+      } else {
+          header.classList.remove('fixed');
+      }
+  }
+  window.addEventListener('scroll', fixHeaderOnScroll);
+  fixHeaderOnScroll();
+}
   
-//     if (window.scrollY >= 1) {
-//         header.classList.add('fixed');
-//     } else {
-//         header.classList.remove('fixed');
-//     }
-// }
-  
-// window.addEventListener('scroll', fixHeaderOnScroll);
-// fixHeaderOnScroll();
 
 // // Header menu
-// const headerBurger = document.querySelector('.header-burger');
-// let scrollPosition = 0;
+const headerBurger = document.querySelector('.header-burger');
+let scrollPosition = 0;
 
-// headerBurger.addEventListener('click', () => {
-//     if (document.body.classList.contains('menu-is-open')) {
-//         document.body.classList.remove('menu-is-open');
-//         window.scrollTo({top: scrollPosition, behavior: 'auto'});
-//     } else {
-//         scrollPosition = window.scrollY;
-//         document.body.classList.add('menu-is-open');
-//     }
-// });
-  
-// window.addEventListener('resize', () => {
-//     document.body.classList.remove('menu-is-open');
-// });
+if(headerBurger){
 
-
+  headerBurger.addEventListener('click', () => {
+      if (document.body.classList.contains('menu-is-open')) {
+          document.body.classList.remove('menu-is-open');
+          window.scrollTo({top: scrollPosition, behavior: 'auto'});
+      } else {
+          scrollPosition = window.scrollY;
+          document.body.classList.add('menu-is-open');
+      }
+  });
+    
+  window.addEventListener('resize', () => {
+      document.body.classList.remove('menu-is-open');
+  });
+}
 
 // Swipper slider
-
-var menu = ['Lunteren', 'Garderen', 'Lunteren', 'Garderen']
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
+    slidesPerView: 1,
+    centeredSlides: true,
+    loopedSlides: 4,
+    loop: false,
 
     pagination: {
       el: '.swiper-pagination',
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '"><p>' + (menu[index]) + '</p></span>';
-      },
     },
 });
+
+var thumbs = new Swiper ('.tabs', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: false,
+  slideToClickedSlide: true,
+});
+
+swiper.controller.control = thumbs;
+thumbs.controller.control = swiper;
 
 const swiper2 = new Swiper('.swiper2', {
     direction: 'horizontal',
     loop: false,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
 
     breakpoints: {
         1024: {
             slidesPerView: 2
+          }
+    },
+  
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+   pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+// slider image text
+const swiper3 = new Swiper('.swiper3', {
+    direction: 'horizontal',
+    loop: true,
+
+    breakpoints: {
+      slidesPerView: 2,
+        1024: {
+            slidesPerView: 4
           }
     },
   
